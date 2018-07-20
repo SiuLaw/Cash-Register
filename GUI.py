dce4246e00e9abb27aeca9b3fac45ffec5e84814
SiuLaw <lawsiufung@me.com>
2018-07-20 10:32:24 +0100

    GUI update

--- GUI.py
+++ GUI.py
@@ -1,66 +1,14 @@
+from imported_functions import take_csv_return_table, find_match_in_table_with_index, file_initialization, get_time
+
 import os
-import time
-import csv
-from tkinter import *
-from tkinter.messagebox import *
+
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+
 
 dir_path = os.path.dirname(__file__)
 os.chdir(dir_path)
-
-def take_csv_return_table( csv_name ):
-    File = open( csv_name,'r')
-    Table = []
-    FileCSV = csv.reader(File, delimiter=",")
-    for line in FileCSV:
-        Table.append(line)
-    File.close()
-    return Table
-
-def make_double_digit( input_val ):
-    if( input_val < 10 ):
-        return "0" + str( input_val )
-    else:
-        return str( input_val )
-
-def find_match_in_table_with_index(match, table, offset_int):
-    for row in table:
-        if( row[0] == match ):
-            return row[offset_int]
-    return "ERR"
-
-def get_time():
-    localtime = time.localtime(time.time())
-    hour = str(localtime[3])
-    minute = make_double_digit( localtime[4] )
-
-    this_time = hour + ":" + minute
-    
-    return this_time
-
-def file_initialization():
-    this_time = time.localtime(time.time())
-    year = str(this_time[0])
-    mon = make_double_digit( this_time[1] )
-    day = make_double_digit( this_time[2] )
-    
-    this_date = year + "-" + mon + "-" + day
-    record_file_name = "Report-" + this_date + ".csv"
-    
-    file = open(record_file_name,"a+")
-    file.close()
-    
-    file = open(record_file_name,"r+")
-    if file.read() == "":
-        file.close()
-        
-        file = open(record_file_name,"a+")
-        file.write( "Time" + "," + "Person" + "," + "Franchise" + "," + "Merch. Type" + "," + "Detail" + "," + "Price" + "\n" )
-        file.close()
-        
-    else:
-        file.close()
-        
-    return record_file_name
 
 def update_price():
     pri_v.set( int( find_match_in_table_with_index( typ_v.get(), pri_table, 2 ) ))
@@ -75,7 +23,7 @@ pri_table = take_csv_return_table( 'PriceInput.txt' )
 
 
 
-root = Tk()
+root = tk.Tk()
 
 root.title("Cashier")
 root.resizable(width=False, height=False) #do not let outer window be resized
@@ -83,40 +31,40 @@ root.resizable(width=False, height=False) #do not let outer window be resized
 row_names = ["Person","Fandom","Type","Detail","Price(GBP)"]
 r = 0
 for row_name in row_names:
-    Label(root, text=row_name,relief=SUNKEN,width = 10).grid(row=r,column=0)
+    tk.Label(root, text=row_name,relief=tk.SUNKEN,width = 10).grid(row=r,column=0)
     r += 1
     
     
 
-per_frame = Frame(root)
-fan_frame = Frame(root)
-typ_frame = Frame(root)
-pri_frame = Frame(root)
-sub_frame = Frame(root)
-ent_frame = Frame(root)
+per_frame = tk.Frame(root)
+fan_frame = tk.Frame(root)
+typ_frame = tk.Frame(root)
+pri_frame = tk.Frame(root)
+sub_frame = tk.Frame(root)
+ent_frame = tk.Frame(root)
 
-per_v = StringVar()
+per_v = tk.StringVar()
 per_v.set(per_table[0][0])
-fan_v = StringVar()
+fan_v = tk.StringVar()
 fan_v.set(fan_table[0][0])
-typ_v = StringVar()
+typ_v = tk.StringVar()
 typ_v.set(pri_table[0][0])
 
-pri_v = IntVar()
+pri_v = tk.IntVar()
 pri_v.set(-1)
 
-alt_v = IntVar()
+alt_v = tk.IntVar()
 alt_v.set(1)
 
 
 
 
 for short_name, long_name in per_table:
-    Radiobutton(per_frame, text=long_name, variable=per_v, value=short_name).pack(side=LEFT)
+    tk.Radiobutton(per_frame, text=long_name, variable=per_v, value=short_name).pack(side=tk.LEFT)
     
-Radiobutton(per_frame, text="Other: ", variable=per_v, value="CUS").pack(side=LEFT)
-per_entry = Entry(per_frame)
-per_entry.pack(side=LEFT)
+tk.Radiobutton(per_frame, text="Other: ", variable=per_v, value="CUS").pack(side=tk.LEFT)
+per_entry = tk.Entry(per_frame)
+per_entry.pack(side=tk.LEFT)
 
 def focus_per(event):
     per_v.set("CUS")
@@ -125,10 +73,10 @@ per_entry.bind("<Button-1>",focus_per)
 
 
 for short_name, long_name in fan_table:
-    Radiobutton(fan_frame, text=long_name, variable=fan_v, value=short_name).pack(side=LEFT)
+    tk.Radiobutton(fan_frame, text=long_name, variable=fan_v, value=short_name).pack(side=tk.LEFT)
 
 for short_name, long_name, price in pri_table:
-    Radiobutton(typ_frame, text=long_name, variable=typ_v, value=short_name,command=update_price).pack(side=LEFT)
+    tk.Radiobutton(typ_frame, text=long_name, variable=typ_v, value=short_name,command=update_price).pack(side=tk.LEFT)
 
 def green_f1():
     f1.config(bg="green")
@@ -149,17 +97,17 @@ def green_f2():
     pri_def.config(bg="white")
     pri_def_label.config(bg="white")
 
-f1 = Frame(pri_frame,bg="green")
-pri_def = Radiobutton(f1,text="Default price:", variable=alt_v, value=1,bg="green",command=green_f1)
-pri_def_label = Label(f1,textvariable=pri_v,bg="green")
-pri_def.pack(side=LEFT)
-pri_def_label.pack(side=LEFT)
+f1 = tk.Frame(pri_frame,bg="green")
+pri_def = tk.Radiobutton(f1,text="Default price:", variable=alt_v, value=1,bg="green",command=green_f1)
+pri_def_label = tk.Label(f1,textvariable=pri_v,bg="green")
+pri_def.pack(side=tk.LEFT)
+pri_def_label.pack(side=tk.LEFT)
 
-f2 = Frame(pri_frame,bg="white")
-pri_alt = Radiobutton(f2,text="Alter. price:", variable=alt_v, value=2,command=green_f2,bg="white")
-pri_alt_entry = Entry(f2,bg="white")
-pri_alt.pack(side=LEFT)
-pri_alt_entry.pack(side=LEFT)
+f2 = tk.Frame(pri_frame,bg="white")
+pri_alt = tk.Radiobutton(f2,text="Alter. price:", variable=alt_v, value=2,command=green_f2,bg="white")
+pri_alt_entry = tk.Entry(f2,bg="white")
+pri_alt.pack(side=tk.LEFT)
+pri_alt_entry.pack(side=tk.LEFT)
 
 def focus_pri(event):
     alt_v.set(2)
@@ -168,7 +116,7 @@ pri_alt_entry.bind("<Button-1>",focus_pri)
 
 
 
-f3 = Label(pri_frame, text="  /  ")
+f3 = tk.Label(pri_frame, text="  /  ")
 
 f1.grid(row=0,column=0)
 f3.grid(row=0,column=1)
@@ -177,7 +125,7 @@ f2.grid(row=0,column=2)
 
 
 
-detail_entry = Entry(ent_frame)
+detail_entry = tk.Entry(ent_frame)
 detail_entry.pack()
 
 def submit_record():
@@ -198,15 +146,15 @@ def submit_record():
     detail = detail_entry.get()
     output = this_time + ',' + person + ',' + fandom + ',' + mertyp + ',' + detail + ',' + str(prices)
     
-    if askokcancel('Confirm?', output):
+    if tkm.askokcancel('Confirm?', output):
         file = open( record_file_name, "a")
         file.write( output + "\n" )
         file.close()
 
-        showinfo('Submitted', 'Record submitted')
+        tkm.showinfo('Submitted', 'Record submitted')
     
 
-Button(sub_frame,text="Submit",command=submit_record).pack()
+tk.Button(sub_frame,text="Submit",command=submit_record).pack()
 
 
 
---
+++ imported_functions.py
@@ -0,0 +1,64 @@
+import csv
+import time
+
+def foo():
+    print("foo")
+
+def take_csv_return_table( csv_name ):
+    File = open( csv_name,'r')
+    Table = []
+    FileCSV = csv.reader(File, delimiter=",")
+    for line in FileCSV:
+        Table.append(line)
+    File.close()
+    return Table
+
+def mdd( input_val ):
+    output_val = "-1"
+    if( input_val < 10 ):
+        output_val = "0" + str( input_val )
+    else:
+        output_val = input_val
+        
+    return str(output_val)
+    return input_val
+    
+def find_match_in_table_with_index(match, table, offset_int):
+    for row in table:
+        # if( row[0] == match ):
+            return row[offset_int]
+    return "ERR"
+    
+def get_time():
+    localtime = time.localtime(time.time())
+    hour = str(localtime[3])
+    minute = mdd( localtime[4] )
+
+    this_time = hour + ":" + minute
+    
+    return this_time
+
+def file_initialization():
+    this_time = time.localtime(time.time())
+    year = str(this_time[0])
+    mon = mdd( this_time[1] )
+    day = mdd( this_time[2] )
+    
+    this_date = year + "-" + mon + "-" + day
+    record_file_name = "Report-" + this_date + ".csv"
+    
+    file = open(record_file_name,"a+")
+    file.close()
+    
+    file = open(record_file_name,"r+")
+    if file.read() == "":
+        file.close()
+        
+        file = open(record_file_name,"a+")
+        file.write( "Time" + "," + "Person" + "," + "Franchise" + "," + "Merch. Type" + "," + "Detail" + "," + "Price" + "\n" )
+        file.close()
+        
+    else:
+        file.close()
+        
+    return record_file_name
