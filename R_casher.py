0aff3d7a5a64c682e6e53dfa842873bcf31e16cd
TableFork <besttablefork@gmail.com>
2018-08-12 13:11:05 +0100

    tidy up

--- R_casher.py
+++ R_casher.py
@@ -10,13 +10,20 @@ import tkinter.font as tkFont
 import tkinter.ttk as ttk
 import sqlite3
 
-dir_path = os.path.dirname(__file__)
-os.chdir(dir_path)
+dir_path = os.path.dirname( __file__ )
+os.chdir( dir_path )
 
-stockItemAttr = ["seller","fandom","maintype","bundle","price","details","stock"]
+stockItemAttr = [ "seller", "fandom", "maintype","bundle","price","details","stock"]
 
 ############################################################################################################################
-#FUNCTION
+#NOTE
+
+#lambda
+    # one time code
+    # BLUEPRINT = lambda argument: manipulation(argument)
+    
+############################################################################################################################
+#FUNCTIONS
 
 # Converting empty input into default value
 def inputOrDefault( inp_val, def_val ):
@@ -26,7 +33,7 @@ def inputOrDefault( inp_val, def_val ):
         return inp_val
 
 # Read csvfile -> list -> objects
-def importObject(csvfile , objectClass):
+def importObject( csvfile, objectClass ):
     # initialize the objectList for output
     objectList = [ ]
     
@@ -36,16 +43,16 @@ def importObject(csvfile , objectClass):
         return objectList
     
     # Open csvfile, read and store everything into [table]
-    file = open( csvfile ,'r')
-    spamreader = csv.reader( file , delimiter = ",")
-    table = []
+    file = open( csvfile, 'r')
+    spamreader = csv.reader( file, delimiter = ",")
+    table = [ ]
     for line in spamreader:
-        table.append(line)
+        table.append( line )
     
     # Create a list of objects
     col_title_passed = False
-    for i in range( len(table) ):
-        line = table[i]
+    for i in range( len( table ) ):
+        line = table[ i ]
         if( col_title_passed == False ):
             col_title_passed = True
             continue
@@ -57,26 +64,26 @@ def importObject(csvfile , objectClass):
     return objectList
 
 # Create list of objects
-def objectintoList (x,objectClass):
-    olist = []
+def objectintoList ( x, objectClass ):
+    olist = [ ]
     for things in x:
-        if isinstance(things,objectClass):
-            olist.append(things)
-    olist = sortList(olist)
+        if isinstance( things, objectClass ):
+            olist.append( things )
+    olist = sortList( olist )
     return olist
 
 # input stock + renew stockList
-def stockInput():
-    Input('stock.csv',stockItem,stockItemAttr)
-    stockList = importObject('stock.csv',stockItem)
-    MLB(root,stockList)
+def stockInput( ):
+    Input( 'stock.csv', stockItem, stockItemAttr )
+    stockList = importObject( 'stock.csv', stockItem )
+    MLB( root, stockList )
 
 # Part of input
 def listToCSVtxt( input_list ):
     output_text = ""
     length = len( input_list )
     for i in range( length ):
-        output_text += input_list[i]
+        output_text += input_list[ i ]
         if i < length - 1:
             output_text += ","
         else:
@@ -84,47 +91,48 @@ def listToCSVtxt( input_list ):
     return output_text 
 
 # Input -> list -> stored into csv file
-def Input(csvfile,objectClass,ItemAttr): 
-    newAttr = []
+def Input( csvfile, objectClass, ItemAttr ): 
+    newAttr = [ ]
     i = 0
-    newObject = objectClass()
-    objectAttrLength = newObject.lenAttr()
+    newObject = objectClass( )
+    objectAttrLength = newObject.lenAttr( )
     
     #turning input into list
     while i < objectAttrLength:
-        newAttr.append(boxes[i].get() )
+        newAttr.append( boxes[ i ].get( ) )
         i +=1
     
     #turning storing list via listToCSVtxt
-    if( os.path.isfile(csvfile) == False ):
-            file = open(csvfile,"a+")
+    if( os.path.isfile( csvfile ) == False ):
+            file = open( csvfile, "a+" )
             text = listToCSVtxt( ItemAttr ) 
-            file.write(text) 
+            file.write( text ) 
             file.close
             
-    file = open(csvfile,"a+")
+    file = open( csvfile, "a+" )
     text = listToCSVtxt( newAttr )
     file.write( text )
     file.close
 
-def sortList(objectList):
-    objectList_sorted = sorted(objectList,key = operator.attrgetter('maintype'))
-    objectList_sorted = sorted(objectList,key = operator.attrgetter('fandom'))
-    objectList_sorted = sorted(objectList,key = operator.attrgetter('seller'))
+def sortList( objectList ):
+    objectList_sorted = sorted( objectList, key = operator.attrgetter( 'maintype' ))
+    objectList_sorted = sorted( objectList, key = operator.attrgetter( 'fandom' ))
+    objectList_sorted = sorted( objectList, key = operator.attrgetter( 'seller' ))
     return objectList_sorted
 
 # Sort content when header is clicked on in MLB
-def sortby(tree,col,descending):
+def sortby( tree, col, descending ):
     # Making a list of [column,content]
-    data = [(tree.set(child, col), child) \
-        for child in tree.get_children('')]
-    data.sort(reverse=descending)
+        # tree.set = returns value of the specific column
+        # tree.get_children = returns list of children belonging to items
+    data = [( tree.set( child, col ), child ) for child in tree.get_children('')]
     
-    for ix, item in enumerate(data):
-        tree.move(item[1], '', ix)
-        
-    tree.heading(col, command=lambda col=col: sortby(tree, col, \
-        int(not descending)))
+    data.sort( reverse = descending )
+    for ix, item in enumerate( data ):
+        tree.move( item[ 1 ], '', ix ) # tree.move(item,parent,index), move #item# to position #index# in #parent's# list of children
+    
+    # tree.heading(column, command = reversed sortby)
+    tree.heading( col, command = lambda col = col: sortby( tree, col, int( not descending ) ) )
 
 def printObjectList( objectList ):
     for obj in objectList:
@@ -133,88 +141,66 @@ def printObjectList( objectList ):
 ############################################################################################################################
 #GUI FUNCTION
 
-# def display_stock(root,stockList):
-#     #For displaying stock
-#     i = 0
-#     lists = []
-#     n = stockItemAttrLength
-#     
-#     #while loop to create enough boxes for all stockItemAttr
-#     while i < n: 
-#         list_stock = Listbox(root) 
-#         list_stock.grid(row = 2,column = i) 
-#         lists.append(list_stock)
-#         q = 0
-#     
-#         #inserting attribute of each imputed objects into listbox
-#         while q < len(stockList): #
-#             for things in stockList:
-#                 attribute = stockList[q]
-#                 list_stock.insert(END, attribute.switcher(i))
-#                 q += 1
-#         i += 1
-#     print( "stock display updated" )
-
 #Multi List Box
-def MLB(root,stockList):
+def MLB( root, stockList ):
     # Container created in loop because for updating stocklist
     
     # Constructing Tree
     i = 0
-    title = []
+    title = [ ]
     while i < stockItemAttrLength:
-        title.append( str(stockItemAttr[i]).capitalize()) 
+        title.append( str( stockItemAttr[ i ] ).capitalize( ) ) 
         i += 1
-    tree = ttk.Treeview(column = title, show = "headings")
+    tree = ttk.Treeview( column = title, show = "headings" )
     
     # Vertical scrollbar
-    vsb = ttk.Scrollbar(orient="vertical", command = tree.yview)
-    tree.configure(yscrollcommand=vsb.set)
-    tree.grid(row = 0, sticky='nsew', in_ = container)
-    vsb.grid(row = 0, column = 1, sticky='ns',in_ = container)
+    vsb = ttk.Scrollbar( orient="vertical", command = tree.yview )
+    tree.configure( yscrollcommand = vsb.set )
+    tree.grid( row = 0, column = 0, sticky ='nsew', in_ = container)
+    vsb.grid ( row = 0, column = 1, sticky='ns'   , in_ = container)
 
-    container.grid_columnconfigure(0, weight=1)
-    container.grid_rowconfigure(0, weight=1)
+    container.grid_columnconfigure( 0, weight = 1 )
+    container.grid_rowconfigure   ( 0, weight = 1 )
     
     # Filling in listbox
     i = 0
-    while i < len(stockList): 
+    while i < len( stockList ): 
         q = 0
         while q < stockItemAttrLength: #
-            stuff = []
+            stuff = [ ]
             for things in stockList:
-                stuff.append(stockList[i].switcher(q))
+                stuff.append( stockList[ i ].switcher( q ) )
                 q += 1
-            tree.insert('','end', values = stuff)
+            tree.insert( '', 'end', values = stuff )
         i += 1
     print( "stock display updated" )
     
+    # Title
+        # col.title = assign headings
+        # command = sortby(tree,column, descending = 0)
     for col in title:
-        tree.heading(col, text=col.title(),
-            command=lambda c=col: sortby(tree, c, 0))
-        tree.column(col,
-            width=tkFont.Font().measure(col.title()))
+        tree.heading( col, text = col.title( ), command = lambda c = col: sortby( tree, c, 0 ) )
 
 def entries(root):
-    container = Frame (bg = "white")
-    container.pack(fill = "both",expand = True)
+    container = Frame ( bg = "white" )
+    container.pack( fill = "both", expand = True )
     
     #Creating labels
     i = 0
     while i < stockItemAttrLength:
-        title = str(stockItemAttr[i]).capitalize() 
-        Label(root,text = title).grid(row = 0,column = i,in_ = container)
-        Label(root,text = title).grid(row = 0,column = i,in_ = container)
+        title = str( stockItemAttr[ i ] ).capitalize( ) 
+        Label( root, text = title ).grid( row = 0, column = i, in_ = container )
+        Label( root, text = title ).grid( row = 0, column = i, in_ = container )
         i += 1
     
     #Creating entries boxes
     i = 0
-    boxes = []
+    boxes = [ ]
     while i < stockItemAttrLength:
-        entry = Entry(root)
-        entry.grid(row = 1, column = i,in_ = container)
-        istr = str(i)
-        boxes.append(entry)
+        entry = Entry( root )
+        entry.grid( row = 1, column = i, in_ = container )
+        istr = str( i )
+        boxes.append( entry )
         i += 1
     return boxes
 
@@ -224,7 +210,7 @@ def entries(root):
 #MAIN CLASS
 class Item:
     
-    def __init__(self, *args): #*args = for any number of arguments
+    def __init__( self, *args ): #*args = for any number of arguments
         # DEFAULT VALUES
         self.seller   = "Unknown seller"
         self.fandom   = "Unknown fandom"
@@ -233,58 +219,58 @@ class Item:
         self.price    = 0
         self.details  = "-"
         
-        if len(args) >= 6:
+        if len( args ) >= 6:
             # When the args are input as individually ( i.e. Item( seller, fandom, maintype, bundle, price, details ) )
-            self.initializeFromArgs(*args)
-        elif len(args) == 1:
+            self.initializeFromArgs( *args )
+        elif len( args ) == 1:
             # When there is only one input, then check that it is a list
-            if( isinstance( args[0], list) ):
-                self.initializeFromList(args[0])
+            if( isinstance( args[ 0 ], list ) ):
+                self.initializeFromList( args[ 0 ] )
             else:
-                print("Only one input and not list")
+                print( "Only one input and not list" )
         else:
             pass
             
     # Set the attr values with the 6 arguments
-    def initializeFromArgs(self,*args):
+    def initializeFromArgs( self, *args ):
 
-        self.seller   = inputOrDefault( args[0],"Unknown seller" )
-        self.fandom   = inputOrDefault( args[1],"Unknown fandom" )
-        self.maintype = inputOrDefault( args[2],"Unknown type" )
-        self.bundle   = inputOrDefault( args[3],"Unknown bundle" )
-        self.price    = inputOrDefault( args[4],0 ) 
-        self.details  = inputOrDefault( args[5],"-" )
+        self.seller   = inputOrDefault( args[0], "Unknown seller" )
+        self.fandom   = inputOrDefault( args[1], "Unknown fandom" )
+        self.maintype = inputOrDefault( args[2], "Unknown type" )
+        self.bundle   = inputOrDefault( args[3], "Unknown bundle" )
+        self.price    = inputOrDefault( args[4], 0 ) 
+        self.details  = inputOrDefault( args[5], "-" )
         
     # Set the attr values with the single "list" arguments
-    def initializeFromList(self,argList):
-        self.seller   = inputOrDefault( argList[0],"Unknown seller" )
-        self.fandom   = inputOrDefault( argList[1],"Unknown fandom" )
-        self.maintype = inputOrDefault( argList[2],"Unknown type" )
-        self.bundle   = inputOrDefault( argList[3],"Unknown bundle" )
-        self.price    = inputOrDefault( argList[4],0 ) 
-        self.details  = inputOrDefault( argList[5],"-" )
+    def initializeFromList( self, argList ):
+        self.seller   = inputOrDefault( argList[0], "Unknown seller" )
+        self.fandom   = inputOrDefault( argList[1], "Unknown fandom" )
+        self.maintype = inputOrDefault( argList[2], "Unknown type" )
+        self.bundle   = inputOrDefault( argList[3], "Unknown bundle" )
+        self.price    = inputOrDefault( argList[4], 0 ) 
+        self.details  = inputOrDefault( argList[5], "-" )
     
     # Amount of attribute, for number of loops needed
-    def lenAttr(self):
-        attrLength = len(list(self.__dict__.keys()))
+    def lenAttr( self ):
+        attrLength = len( list( self.__dict__.keys( ) ) )
         return attrLength
     
 #SUB CLASS
-class stockItem(Item):
+class stockItem( Item ):
     
-    def __init__(self,*args):
+    def __init__( self, *args ):
         self.stock = "0"
-        super().__init__(*args) #inherit all arguments from class Item
+        super( ).__init__( *args ) #inherit all arguments from class Item
         
         # Dealing with the extra input of "stock", unique to the stockItem class
-        if( len(args) == 7 ):
-            self.stock = inputOrDefault( args[6], 0 )
-        elif len(args) == 1:
-            if isinstance( args[0], list):
-                self.stock = inputOrDefault( args[0][6], 0 )
+        if( len( args ) == 7 ):
+            self.stock = inputOrDefault( args[ 6 ], 0 )
+        elif len( args ) == 1:
+            if isinstance( args[ 0 ], list ):
+                self.stock = inputOrDefault( args[ 0 ][ 6 ], 0 )
     
     #calling attributes with number
-    def switcher(self,number):
+    def switcher( self, number ):
         switcher = {
             0: self.seller,
             1: self.fandom,
@@ -294,53 +280,66 @@ class stockItem(Item):
             5: self.details,
             6: self.stock
         }
-        return switcher.get(number, 0)
+        return switcher.get( number, 0 )
     
-    def sales(self):
-        self.stock = int(self.stock) - 1
+    def sales( self ):
+        self.stock = int( self.stock ) - 1
         
-class salesItem(Item):
+class salesItem( Item ):
     
-    def __init__(self,seller = "Unknown seller", fandom="Unknown fandom",maintype = "Unknown type",bundle = "Unknown bundle",price = 0,details = "-", discount = False, alternativePrice = 0):
-        super().__init__(seller,fandom,maintype,bundle,price,details)
-        self.discount = discount
-        self.alternativePrice = alternativePrice
+    def __init__( self, *args ):
+        self.discount = False
+        self.alternativePrice = 0
+        super().__init__( *args )
+        
+    def switcher( self, number ):
+        switcher = {
+            0: self.seller,
+            1: self.fandom,
+            2: self.maintype,
+            3: self.bundle,
+            4: self.price,
+            5: self.details,
+            6: self.discount,
+            7: self.alternativePrice
+        }
+        return switcher.get( number, 0 )
 
 #For grabbing stuff from 
-def readFile(x):
-    file = open(x,"r")
+def readFile( x ):
+    file = open( x , "r" )
   
 ############################################################################################################################
 #DEFAULT ITEMS
 
 #Deafult StockItem
-defaultStockItem = stockItem()
-stockItemAttrLength = defaultStockItem.lenAttr()
+defaultStockItem = stockItem( )
+stockItemAttrLength = defaultStockItem.lenAttr( )
 del defaultStockItem
 
 ############################################################################################################################
 #OBJECT INPUT
 #Object into list
-objectList = importObject('stock.csv',stockItem)
-allObject = gc.get_objects()
-stockList = objectintoList (allObject,stockItem)
+objectList = importObject( 'stock.csv', stockItem )
+allObject = gc.get_objects( )
+stockList = objectintoList ( allObject, stockItem )
 ############################################################################################################################
 #TKinter
-root = Tk()
+root = Tk( )
 
-Label(root, text='Stock list').pack()
+Label( root, text = 'Stock list' ).pack( )
 
-container = ttk.Frame()
-container.pack(fill = "both",expand = True)
-MLB(root,stockList)
+container = ttk.Frame( )
+container.pack( fill = "both", expand = True )
+MLB( root, stockList )
 
-LINE = Frame(root,height=2,width=5000,bg="black")
-LINE.pack()
+LINE = Frame( root, height = 2, width = 5000, bg = "black" )
+LINE.pack( )
 
-Label(root, text='New Entries').pack(side = "left")
+Label( root, text = 'New Entries' ).pack( side = "left" )
 
-boxes = entries(root)
-Button(root,text = "Input", command=stockInput).pack()
+boxes = entries( root )
+Button( root, text = "Input", command = stockInput ).pack( )
 
-mainloop()
+mainloop( )
 ############################################################################################################################
